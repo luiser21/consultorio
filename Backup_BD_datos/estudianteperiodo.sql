a1143e1f1cbecf3f0cb954e6436a272a9a104563
luiser21 <luishernando18@hotmail.com>
2013-11-14 22:15:23 -0500

    nuevos datos

--- Backup_BD_datos/estudianteperiodo.sql
+++ Backup_BD_datos/estudianteperiodo.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
--- Started on 2013-09-24 11:07:24 Hora est. del Pacífico de SA
+-- Started on 2013-11-14 15:55:02 Hora est. del Pacífico de SA
 
 SET client_encoding = 'SQL_ASCII';
 SET check_function_bodies = false;
@@ -51,7 +51,7 @@ INSERT INTO estudianteperiodo (codigo, estudiante, periodo) VALUES (34, 43, '220
 INSERT INTO estudianteperiodo (codigo, estudiante, periodo) VALUES (35, 55, '22011');
 
 
--- Completed on 2013-09-24 11:07:24 Hora est. del Pacífico de SA
+-- Completed on 2013-11-14 15:55:02 Hora est. del Pacífico de SA
 
 --
 -- PostgreSQL database dump complete
